08c364b780f701e67c91e007fc96089944fb6be8
Richard Farr <richard.farr@okta.com>
2020-01-31 14:51:42 -0500

    Remove mut pattern for 2 functions without body
      - Addresses https://github.com/rust-lang/rust/issues/35203

--- src/owned.rs
+++ src/owned.rs
@@ -120,7 +120,7 @@ pub trait RedisClientOwned: Sized {
 
   fn hmget<F: Into<MultipleKeys>, K: Into<RedisKey>>(self, key: K, fields: F) -> Box<Future<Item=(Self, Vec<RedisValue>), Error=RedisError>>;
 
-  fn hmset<V: Into<RedisValue>, F: Into<RedisKey> + Hash + Eq, K: Into<RedisKey>>(self, key: K, mut values: HashMap<F, V>) -> Box<Future<Item=(Self, String), Error=RedisError>>;
+  fn hmset<V: Into<RedisValue>, F: Into<RedisKey> + Hash + Eq, K: Into<RedisKey>>(self, key: K, values: HashMap<F, V>) -> Box<Future<Item=(Self, String), Error=RedisError>>;
 
   fn hset<K: Into<RedisKey>, F: Into<RedisKey>, V: Into<RedisValue>>(self, key: K, field: F, value: V) -> Box<Future<Item=(Self, usize), Error=RedisError>>;
 
